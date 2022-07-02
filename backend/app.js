b3c28cb3a9d376647dcf0246ca181b82203a9454
Chathura Madhusanka <cmadhusanka29@gmail.com>
2022-07-02 18:36:51 +0530

    File Structure

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+//Middlewares
+app.use('/', ())
+mongoose
+  .connect(
+    "mongodb+srv://admin:Madhusanka331@cluster0.dexvsy2.mongodb.net/Book_StoreDB?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Database Connected"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch(() => console.log(err));
