aaddbb166170460075f253d29678b9da1d5fcf5d
Chathura Madhusanka <cmadhusanka29@gmail.com>
2022-07-05 00:59:21 +0530

    book crud

--- book-store/src/components/About.js
+++ book-store/src/components/About.js
@@ -1,8 +1,13 @@
+import { Box } from '@mui/material'
 import React from 'react'
 
 const About = () => {
   return (
-    <div>This is about us page</div>
+    <div>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        
+      </Box>
+    </div>
   )
 }
 
