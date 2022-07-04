dc34d4b68fcec399fb964e179a422b1a7aa1cd02
Chathura Madhusanka <cmadhusanka29@gmail.com>
2022-07-05 01:23:05 +0530

    Application Done

--- book-store/src/components/About.js
+++ book-store/src/components/About.js
@@ -1,11 +1,13 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 const About = () => {
   return (
     <div>
       <Box display="flex" flexDirection="column" alignItems="center">
-        
+        <Typography sx={{fontFamily: "fantacy"}} variant ="h2">
+          This Application Is A Book Store Crud Application 
+        </Typography>
       </Box>
     </div>
   )
